87a2a1d1841ca2017355294f61fe1ebb5e432ab0
vishal-Tx <vishalguptaa2z111@gmail.com>
2023-02-13 23:52:51 +0530

    info component added

--- src/components/Info.tsx
+++ src/components/Info.tsx
@@ -4,8 +4,8 @@ import { gql, useQuery } from "@apollo/client";
 import "../App.css";
 
 const FETCH_COUNTRY_DETAILS = gql`
-  query Country($search: string!) {
-    country(code: $search) {
+  query Country($code: ID!) {
+    country(code: $code) {
       name
       native
       capital
@@ -21,14 +21,50 @@ const FETCH_COUNTRY_DETAILS = gql`
 
 const Info = () => {
   const { search } = useParams();
-  const { data, loading, error } = useQuery(FETCH_COUNTRY_DETAILS);
-  console.log(data);
+  const { data, loading, error } = useQuery<{ country: Country }>(
+    FETCH_COUNTRY_DETAILS,
+    {
+      variables: { code: search },
+    }
+  );
+  console.log("error", error);
+  console.log("loading", loading);
+  console.log("data", data);
 
-  console.log("search", search);
+  // console.log("search", search);
   return (
     <div className="info">
-      Info
-      <Link to="/">Home</Link>
+      <div className="navbar">
+        <h1>Info</h1>
+      </div>
+      <div className="link-home">
+        <Link to="/" style={{ textDecoration: "none" }}>
+          Home
+        </Link>
+      </div>
+      <div className="info-container">
+        {loading && <h1>Loading...</h1>}
+        {error && <h1>Something went wrong</h1>}
+        {data && (
+          <>
+            <h1>
+              {data?.country.name} {data?.country.emoji}
+            </h1>
+            <h2>Native: {data?.country.native}</h2>
+            <h2>Capital: {data?.country.capital}</h2>
+
+            <h2>Currency: {data?.country.currency}</h2>
+            <ul>
+              <h2>Languages:</h2>
+              {data?.country.languages.map((language) => (
+                <li className="lang-item" key={language.code}>
+                  {language.name}
+                </li>
+              ))}
+            </ul>
+          </>
+        )}
+      </div>
     </div>
   );
 };
